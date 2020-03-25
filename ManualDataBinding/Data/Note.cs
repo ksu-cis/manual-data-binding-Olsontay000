97b0178b9040b77619f6f5efadd80f20839dfed5
Taylor Olson <olsontay000@ksu.edu>
2020-03-25 15:51:36 -0500

    Finished 3/25 Online Lab

--- ManualDataBinding/Data/Note.cs
+++ ManualDataBinding/Data/Note.cs
@@ -7,14 +7,43 @@ namespace ManualDataBinding.Data
     /// </summary>
     public class Note
     {
+        public event EventHandler NoteChanged;
+
+        private string title = DateTime.Now.ToString();
+
+        private string body = "";
         /// <summary>
         /// The title of the Note
         /// </summary>
-        public string Title { get; set; } = DateTime.Now.ToString();
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                if (title == value) return;
+                title = value;
+                NoteChanged?.Invoke(this, new EventArgs());
+            }
+        }
 
         /// <summary>
         /// The text of the note
         /// </summary>
-        public string Body { get; set; } = "";
+        public string Body
+        {
+            get
+            {
+                return body;
+            }
+            set
+            {
+                if (body == value) return;
+                body = value;
+                NoteChanged?.Invoke(this, new EventArgs());
+            }
+        }
     }
 }
